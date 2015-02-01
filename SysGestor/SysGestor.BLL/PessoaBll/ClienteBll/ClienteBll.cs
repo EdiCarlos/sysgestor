bfb40c1948928803de96e43c5e5e83b2df85fa7d
Edi Carlos Simões de Oliveira <edicarlosma@gmail.com>
2015-02-01 17:14:52 -0200

    FornecedorBll Criação

--- SysGestor/SysGestor.BLL/PessoaBll/ClienteBll/ClienteBll.cs
+++ SysGestor/SysGestor.BLL/PessoaBll/ClienteBll/ClienteBll.cs
@@ -11,6 +11,7 @@ namespace SysGestor.BLL
     {
         PessoaDal _pessoaDal;
         ClienteDal _clienteDal;
+
         public ClienteBll()
         {
             _pessoaDal = new PessoaDal();
---
+++ SysGestor/SysGestor.BLL/PessoaBll/FornecedorBll/FornecedorBll.cs
@@ -0,0 +1,67 @@
+﻿using SysGestor.DAL.PessoaDal;
+using SysGestor.DAL.PessoaDal.FornecedorDal;
+using SysGestor.DTO.PessoaDto.FornecedorDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysGestor.BLL.PessoaBll.FornecedorBll
+{
+    public class FornecedorBll
+    {
+        PessoaDal _pessoaDal;
+        FornecedorDal _fornecedorDal;
+
+        public FornecedorBll()
+        {
+            _pessoaDal = new PessoaDal();
+            _fornecedorDal = new FornecedorDal();
+        }
+
+        public int GetIdPessoa()
+        {
+            return _pessoaDal.GetIdPessoa();
+        }
+
+        public void Inserir(FornecedorDto fornecedorDto)
+        {
+            _pessoaDal.Inserir(fornecedorDto);
+            var idPessoa = GetIdPessoa();
+            _fornecedorDal.Inserir(fornecedorDto, idPessoa);
+        }
+
+        public void Alterar(FornecedorDto fornecedorDto, int idPessoa)
+        {
+            _pessoaDal.Alterar(fornecedorDto, idPessoa);
+            _fornecedorDal.Alterar(fornecedorDto);
+        }
+
+        public FornecedorDto GetFornecedor(int id)
+        {
+            return _fornecedorDal.GetFornecedor(id);   
+        }
+
+        public IList<FornecedorDto> FindAll()
+        {
+            IList<FornecedorDto> lista = new List<FornecedorDto>();
+
+            lista = null;
+
+            lista = _fornecedorDal.FindAll();
+
+            return lista;
+        }
+
+        public void Remove(int idFornecedor)
+        {
+            _fornecedorDal.Remove(idFornecedor);
+        }
+
+        public void RemoveMass(int[] idFornecedor)
+        {
+            _fornecedorDal.RemoveMass(idFornecedor);
+        }
+    }
+}
